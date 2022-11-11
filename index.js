690fb3d3c83ba52e1ef158cf2b73a61b9d25a2e3
TRISHAN00 <trishansaha43@gmail.com>
2022-11-11 17:00:19 +0600

    final

---
+++ index.js
@@ -0,0 +1,17 @@
+// random number
+function randomNumber(n) {
+  return Math.floor(Math.random() * n + 1);
+}
+
+// sorting names
+const names = [
+  "Zoe Wang",
+  "Darius Mclean",
+  "Valentin Ortiz",
+  "Declan Krueger",
+  "Annabella Webster",
+];
+
+function sortingName(arr) {
+  return arr.sort();
+}

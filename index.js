6ccf89ac3fecc4c51a8c27843907d029dc039edc
TRISHAN00 <trishansaha43@gmail.com>
2022-11-12 13:35:37 +0600

    vowels

--- index.js
+++ index.js
@@ -15,3 +15,36 @@ const names = [
 function sortingName(arr) {
   return arr.sort();
 }
+
+// sorting number
+const numbers = [5, 4, 8, 6, 1, 2, 3, 7, 9, 10];
+
+function sortingNumber(n) {
+  return n.sort((a, b) => a - b);
+}
+
+// find vowels from any sentence.
+const vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+
+function countValue(sentence) {
+  let count = 0;
+  console.log("Count:", count);
+
+  const letters = Array.from(sentence);
+  console.log("Letter:", letters);
+
+  letters.forEach((value) => {
+    console.log("Value", value);
+
+    if (vowels.includes(value)) {
+      console.log("count if", count);
+
+      count++;
+      console.log("count after if", count);
+      console.log("-----------------------------------");
+    }
+  });
+  return count;
+}
+
+countValue("I love bangladesh");

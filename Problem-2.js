ec937c5371b47577cc7c961e8c6d019e091953ff
TRISHAN00 <trishansaha43@gmail.com>
2022-11-12 16:47:24 +0600

    linearSearch

--- Problem-2.js
+++ Problem-2.js
@@ -4,3 +4,18 @@ const sentence =
 
 const matches = sentence.match(/Trishan/gi);
 const occurrence = matches ? matches.length : 0;
+
+// input: linearSearch (['a', 'b', 'c', 'd'], 'c')
+// output : position or not found
+// problem is implement linearSearch() function
+
+function linearSearch(arr, val) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) {
+      return i;
+    }
+  }
+  return "not found!";
+}
+
+console.log(linearSearch(["a", "b", "c"], "c"));
